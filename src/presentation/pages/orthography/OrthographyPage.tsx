bb22665d127a5ff234260f20796d30d5412cb0e9
Fernando Herrera <fernando.herrera85@gmail.com>
2023-12-18 11:59:55 -0500

    Pagina de ortografía lista

--- src/presentation/pages/orthography/OrthographyPage.tsx
+++ src/presentation/pages/orthography/OrthographyPage.tsx
@@ -1,10 +1,36 @@
+import { useState } from 'react';
 import { GptMessage, MyMessage, TextMessageBox, TypingLoader } from "../../components";
+
+interface Message {
+  text: string;
+  isGpt: boolean;
+}
+
+
+
 
 export const OrthographyPage = () => {
 
+  const [isLoading, setIsLoading] = useState(false);
+  const [messages, setMessages] = useState<Message[]>([])
 
 
-  
+  const handlePost = async( text: string ) => {
+
+    setIsLoading(true);
+    setMessages( (prev) => [...prev, { text: text, isGpt: false }] );
+
+    //TODO: UseCase
+    
+    setIsLoading(false);
+
+    // Todo: Añadir el mensaje de isGPT en true
+
+
+  }
+
+
+
   return (
     <div className="chat-container">
       <div className="chat-messages">
@@ -12,10 +38,27 @@ export const OrthographyPage = () => {
           {/* Bienvenida */}
           <GptMessage text="Hola, puedes escribir tu texto en español, y te ayudo con las correcciones" />
 
-          <MyMessage text="Hola Mundo" />
+          {
+            messages.map( (message, index) => (
+              message.isGpt
+                ? (
+                  <GptMessage key={ index } text="Esto es de OpenAI" />
+                )
+                : (
+                  <MyMessage key={ index } text={ message.text } />
+                )
+                
+            ))
+          }
 
           
-          <TypingLoader className="fade-in" />
+          {
+            isLoading && (
+              <div className="col-start-1 col-end-12 fade-in">
+                <TypingLoader />
+              </div>
+            )
+          }
           
 
         </div>
@@ -23,7 +66,7 @@ export const OrthographyPage = () => {
 
 
       <TextMessageBox 
-        onSendMessage={ (message) => console.log(message) }
+        onSendMessage={ handlePost }
         placeholder='Escribe aquí lo que deseas'
         disableCorrections
       />
